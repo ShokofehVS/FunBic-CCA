b2a79891a493a31cbf4bf5e802c424ae4abc8556
ShokofehVS <sh.vahidian@gmail.com>
2024-09-22 13:10:22 +0200

    For future use cases

--- HumanDataGen.py
+++ HumanDataGen.py
@@ -1,14 +1,6 @@
 import urllib.request
 import numpy as np
 
-# get data
-# url = "http://arep.med.harvard.edu/biclustering/lymphoma.matrix"
-# lines = urllib.request.urlopen(url).read().strip().split('\n')
-# # insert a space before all negative signs
-# lines = list(' -'.join(line.split('-')).split(' ') for line in lines)
-# lines = list(list(int(i) for i in line if i) for line in lines)
-# data = np.array(lines)
-# print(data)
 
 from urllib.request import urlopen
 #check text for curse words
